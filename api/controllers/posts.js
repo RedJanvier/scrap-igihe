8dda2a89c65f281dcf5d261642f0985fa4b15241
RedJanvier <janvierntwali@gmail.com>
2019-10-10 12:28:19 +0200

    [post.js: controllers] added a getTags function in single post route and get all

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,6 +1,10 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+const getTags = (link) => {
+    return link.split('/', 2).filter(l => l !== 'article');
+}
+
 exports.get_all = (req, res, next) => {
     const posts = [];
     request ('https://igihe.com', (error, response, html) => {
@@ -12,13 +16,13 @@ exports.get_all = (req, res, next) => {
                 const time = $(el).find('.time a span').text();
                 const image = 'https://www.igihe.com/' + $(el).find('.homenews a img').attr('data-original');
                 if (typeof url !== 'undefined' && url.length > 0 && title.length) {
-                    const tags = url.split('/', 2).filter(l => l !== 'article');
+                    
                     posts.push({
                         title,
                         image,
                         url,
                         time,
-                        tags
+                        tags: getTags(url)
                     });
                 }
             });
@@ -38,6 +42,7 @@ exports.get_single = (req, res, next) => {
             post['title'] = $('.title-article').text();
             post['date'] = $('.date_x').text();
             post['body'] = $('.text-article .row div:nth-child(3)').text().replace(/\t+/g, '');
+            post['tags'] = getTags(url);
             $('img.artimgv5').each((i, el) => {
                 images.push('https://www.igihe.com/' + $(el).attr('src'));
             });
